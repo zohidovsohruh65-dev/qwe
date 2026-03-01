6aefed48d0b4013cd8de7136bce16732044ba381
zohidovsohruh65-dev <zohidovsohruh65@gmail.com>
2026-03-01 21:42:05 +0500

    Refactor player properties and update event handling

--- server.js
+++ server.js
@@ -9,52 +9,37 @@ let players = {};
 
 io.on('connection', (socket) => {
     players[socket.id] = {
-        x: Math.random() * 500 + 50,
-        y: Math.random() * 400 + 50,
-        angle: 0,
+        x: 300, y: 300,
+        angle: 0, // Угол корпуса
+        turretAngle: 0, // Угол пушки
         hp: 100,
         color: 'hsl(' + Math.random() * 360 + ', 80%, 50%)'
     };
-    io.emit('updatePlayers', players);
-
+    
     socket.on('move', (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
             players[socket.id].angle = data.angle;
-            // Передаем всем остальным, чтобы не было дерготни у себя
+            players[socket.id].turretAngle = data.turretAngle;
+            // Рассылаем экономно
             socket.broadcast.emit('updatePlayers', players);
         }
     });
 
     socket.on('shoot', (data) => {
-        const p = players[socket.id];
-        if (p && p.hp > 0) {
-            io.emit('bullet', {
-                x: p.x + Math.cos(data.angle) * 30,
-                y: p.y + Math.sin(data.angle) * 30,
-                angle: data.angle,
-                owner: socket.id
-            });
-        }
+        io.emit('bullet', { x: data.x, y: data.y, angle: data.angle, owner: socket.id });
     });
 
-    socket.on('hit', (targetId) => {
-        if (players[targetId]) {
-            players[targetId].hp -= 10;
-            if (players[targetId].hp <= 0) {
-                players[targetId].hp = 100;
-                players[targetId].x = Math.random() * 800;
-                players[targetId].y = Math.random() * 600;
-            }
+    socket.on('hit', (id) => {
+        if (players[id]) {
+            players[id].hp -= 10;
+            if (players[id].hp <= 0) { players[id].hp = 100; players[id].x = Math.random()*500; }
             io.emit('updatePlayers', players);
         }
     });
 
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('updatePlayers', players);
-    });
+    socket.on('disconnect', () => { delete players[socket.id]; io.emit('updatePlayers', players); });
 });
 
 http.listen(process.env.PORT || 3000);
